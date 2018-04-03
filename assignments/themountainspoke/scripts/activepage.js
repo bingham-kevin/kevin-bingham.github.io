a7a4560fe413cfa5f79c5fe782e226480163b134
kevin-bingham <kevin@jbingham.net>
2018-04-03 16:50:31 -0700

    term project 1

---
+++ assignments/themountainspoke/scripts/activepage.js
@@ -0,0 +1,25 @@
+var urlString, urlArray, pageHREF, menu, i, currentURL;
+urlString = document.location.href;
+urlArray = urlString.split('/');
+pageHREF = urlArray[urlArray.length - 1];
+
+if (pageHREF !== "") {
+  menu = document.querySelectorAll('ul#primarynav li a');
+  for (i = 0; i < menu.length; i++) {
+    currentURL = (menu[i].getAttribute('href'));
+    menu[i].parentNode.className = '';
+    if (currentURL === pageHREF) {
+      menu[i].parentNode.className = 'active';
+    }
+  }
+}
+if (pageHREF !== "") {
+  menu = document.querySelectorAll('ul#footernav li a');
+  for (i = 0; i < menu.length; i++) {
+    currentURL = (menu[i].getAttribute('href'));
+    menu[i].parentNode.className = '';
+    if (currentURL === pageHREF) {
+      menu[i].parentNode.className = 'active';
+    }
+  }
+}
